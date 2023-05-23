624c80ca69e32415ac734981de0e5b93056b0b55
Hoang Ho <hohuyhoang01011990@gmail.com>
2023-05-23 11:38:11 +0700

    Write a test to build an extension

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,18 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+import { Widget } from '@lumino/widgets';
+
+interface APODResponse {
+  copyright: string;
+  date: string;
+  explanation: string;
+  media_type: 'video' | 'image';
+  title: string;
+  url: string;
+};
+
 /**
  * Initialization data for the myextension extension.
  */
@@ -10,8 +22,67 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'myextension:plugin',
   description: 'A JupyterLab extension.',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [ICommandPalette],
+  activate: async (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension myextension is activated!');
+    console.log('ICommandPalette:', palette);
+    console.log('hihhi:', palette);
+    console.log('heheh:', palette);
+
+    const newWidget = async () => {
+      // Create a blank content widget inside of a MainAreaWidget
+      const content = new Widget();
+      const widget = new MainAreaWidget({ content });
+      widget.id = 'apod-jupyterlab';
+      widget.title.label = 'Astronomy Picture';
+      widget.title.closable = true;
+      // Add an image element to the content
+      let img = document.createElement('img');
+      content.node.appendChild(img);
+
+      // Get a random date string in YYYY-MM-DD format
+      function randomDate() {
+        const start = new Date(2010, 1, 1);
+        const end = new Date();
+        const randomDate = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+        return randomDate.toISOString().slice(0, 10);
+      }
+
+      // Fetch info about a random picture
+      const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${randomDate()}`);
+      const data = await response.json() as APODResponse;
+
+      if (data.media_type === 'image') {
+        // Populate the image
+        img.src = data.url;
+        img.title = data.title;
+      } else {
+        console.log('Random APOD was not a picture.');
+      }
+      return widget;
+    }
+    let widget = await newWidget();
+
+    // Add an application command
+    const command: string = 'apod:open';
+    app.commands.addCommand(command, {
+      label: 'Random Astronomy Picture',
+      execute: async () => {
+        // Regenerate the widget if disposed
+        if (widget.isDisposed) {
+          widget = await newWidget();
+        }
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.add(widget, 'main');
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    // Add the command to the palette.
+    palette.addItem({ command, category: 'Tutorial' });
   }
 };
 
